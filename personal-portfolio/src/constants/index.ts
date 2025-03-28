c6578e765c863bfaf82d38a12797e9317346d87d
Rohan <rohan.sivam2562@gmail.com>
2025-03-28 18:38:32 +1100

    Hero 3d model has been added and scale and position adjusted based on screen size. Adapts to mobile devices

--- personal-portfolio/src/constants/index.ts
+++ personal-portfolio/src/constants/index.ts
@@ -248,8 +248,8 @@ export const navLinks = [
   
   export const calculateSizes = (isSmall, isMobile, isTablet) => {
     return {
-      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
-      deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+      houseScale: isSmall ? 1 : isMobile ? 1 : 1,
+      housePosition: isMobile ? [-190, 20, -9] : [-146, 38, -8],
       cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
       reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
       ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
--- personal-portfolio/src/sections/Hero.tsx
+++ personal-portfolio/src/sections/Hero.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { PerspectiveCamera } from '@react-three/drei'
+import CanvasLoader from '../components/CanvasLoader'
+import { Leva, useControls } from 'leva'
+import ForestHouse from '../components/ForestHouse'
+import { useMediaQuery } from 'react-responsive'
+import { calculateSizes } from '../constants'
+
 
 const Hero = () => {
+    const x = useControls('PokemonRoom',{
+        positionX:{
+            value: -146,
+            min:-300,
+            max:50
+        },
+        positionY:{
+            value: 38,
+            min:-50,
+            max:50
+        },
+        positionZ:{
+            value: -7,
+            min:-50,
+            max:50
+        },
+        rotationX:{
+            value: -7,
+            min:-10,
+            max:10
+        },
+        rotationY:{
+            value: -1.6,
+            min:-10,
+            max:10
+        },
+        rotationZ:{
+            value: -7,
+            min:-10,
+            max:10
+        },
+        scale:{
+            value: 1,
+            min:0.00000001,
+            max:10
+        },
+    })
+    const isMobile = useMediaQuery({maxWidth: 768})
+    const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024})
+    const isSmall = useMediaQuery({maxWidth: 440})
+
+    const sizes = calculateSizes(isSmall,isMobile,isTablet);
   return (
     <section className='min-h-screen w-full flex-col relative'>
         <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
@@ -11,7 +60,13 @@ const Hero = () => {
             </p>
         </div>
         <div className='w-full h-full absolute inset-0'>
-            <Canvas className='w-full h-full'></Canvas>
+            <Leva />
+            <Canvas className='w-full h-full'>
+                <Suspense fallback={<CanvasLoader />}>
+                <PerspectiveCamera makeDefault position={sizes.housePosition} rotation={[-7, -1.6,-7]} scale={sizes.houseScale} />
+                <ForestHouse />
+                </Suspense>
+            </Canvas>
         </div>
 
     </section>
