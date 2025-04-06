6db99a6de4630e84e9a92042c91e9bc15e10b7e9
Rohan <rohan.sivam2562@gmail.com>
2025-04-06 21:00:05 +1000

    Refactored the parallax to use framer motion so that the navbar can now hide and appear based on Y scroll, parallax also affects individual clouds. More clouds are added on mobile to avoid a blank screen

--- personal-portfolio/src/sections/Hero.tsx
+++ personal-portfolio/src/sections/Hero.tsx
@@ -1,17 +1,44 @@
 import Position from '../components/Position'
 import HouseCanvas from '../components/HouseCanvas'
-
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 
 const Hero = ({scrollContainer}) => {
+  const { scrollY } = useScroll();
+
+// Create parallax transforms
+// const starsY = useTransform(scrollY, [0, 500], [0, -50]);
+// const planetsY = useTransform(scrollY, [0, 500], [0, -100]);
+// const mountain1Y = useTransform(scrollY, [0, 500], [0, -150]);
+// const mountain2Y = useTransform(scrollY, [0, 500], [0, -200]);
+// const craterY = useTransform(scrollY, [0, 500], [0, -250]);
+
+const cloudGroups = [
+  { y: useTransform(scrollY, [0, 1000], [-50, -350]), clouds: ['cloud_10.svg', 'cloud_6.svg'] },
+  { y: useTransform(scrollY, [0, 1000], [0, -300]), clouds: ['cloud_2.svg'] },
+  { y: useTransform(scrollY, [0, 1000], [0, -250]), clouds: ['cloud_3.svg'] },
+  { y: useTransform(scrollY, [0, 1000], [50, -200]), clouds: ['cloud_7.svg', 'cloud_5.svg'] },
+  { y: useTransform(scrollY, [0, 1000], [0, -150]), clouds: ['cloud_8.svg'] },
+];
+
+const mobileCloudGroups = [
+  { y: useTransform(scrollY, [0, 700], [0, -250]), clouds: ['cloud_9.svg'] },
+  { y: useTransform(scrollY, [0, 700], [0, -150]), clouds: ['cloud_11.svg'] },
+  { y: useTransform(scrollY, [0, 700], [0, -150]), clouds: ['cloud_1.svg'] },
+  { y: useTransform(scrollY, [0, 700], [0, -150]), clouds: ['cloud_8.svg'] },
+  { y: useTransform(scrollY, [0, 700], [0, -150]), clouds: ['cloud_7.svg'] },
+]
   return (
-    <section className="parallax hero-section">
+    <section className="parallax hero-section h-[100vh]">
       <div className='parallax__content absolute top-[18%] sm:top-[16%] lg:top-[24%] w-full mx-auto lg:pl-[10vh] lg:pr-[30vh] xl:pl-20 xl:pr-72 2xl:px-20 3xl:px-20 flex flex-col lg:flex-row items-start z-10'>
         <div className="flex-1 lg:mb-0">
           <h1 className='font-medium text-white text-[40px] xs:text-[70px] sm:text-[68px] md:text-[80px] lg:text-[100px] 2xl:text-[140px] leading-[110px] 2xl:leading-[160px]'>
-            ROHAN SIVAM
+            ROHAN <br className='hidden xl:block' /> SIVAM
           </h1>
-          <Position />
+          {/* <h1 className='font-medium text-white text-[40px] xs:text-[70px] sm:text-[68px] md:text-[80px] lg:text-[100px] 2xl:text-[140px] leading-[110px] 2xl:leading-[160px]'>
+            SIVAM
+          </h1> */}
+          {/* <Position /> */}
         </div>
         <div className="flex-1 flex justify-start lg:justify-end mt-4 sm:mt-14 ml-8 xs:ml-[-4vh] sm:ml-[-17vh] md:ml-[-26vh] lg:mt-10 2xl:mt-0">
           <div className='font-bold text-[20px] sm:text-[30px] md:text-[36px] 2xl:text-[46px] sm:leading-[40px] md:leading-[50px] 2xl:leading-[60px] streaky-glow max-w-sm 2xl:max-w-lg text-white text-left'>
@@ -19,14 +46,44 @@ const Hero = ({scrollContainer}) => {
           </div>
         </div>
       </div>
+      {/* <motion.img style={{ y: starsY }} className="parallax__stars" src="./parallax/cloud_4.svg" alt="" />
+      <motion.img style={{ y: starsY }} className="parallax__stars" src="./parallax/cloud_6.svg" alt="" />
+      <motion.img style={{ y: planetsY }} className="parallax__planets" src="./parallax/cloud_2.svg" alt="" />
+      <motion.img style={{ y: mountain1Y }} className="parallax__mountain1" src="./parallax/cloud_3.svg" alt="" />
+      <motion.img style={{ y: mountain2Y }} className="parallax__mountain2" src="./parallax/cloud_7.svg" alt="" />
+      <motion.img style={{ y: craterY }} className="parallax__crater" src="./parallax/cloud_8.svg" alt="" />
+      <motion.img style={{ y: mountain2Y }} className="parallax__mountain2" src="./parallax/cloud_5.svg" alt="" /> */}
+      {cloudGroups.map((group, i) =>
+        group.clouds.map((src, j) => (
+          <motion.img
+            key={`${i}-${j}`}
+            style={{ y: group.y }}
+            className={`cloud cloud--group${i + 1}`}
+            src={`./parallax/${src}`}
+            alt=""
+          />
+        ))
+      )}
 
-      <img className="parallax__stars" src="./parallax/clouds_1.svg" alt="" />
+      {mobileCloudGroups.map((group, i) =>
+        group.clouds.map((src, j) => (
+          <motion.img
+            key={`${i}-${j}`}
+            style={{ y: group.y }}
+            className={`cloud mobile-cloud--group${i + 1} lg:hidden`}
+            src={`./parallax/${src}`}
+            alt=""
+          />
+        ))
+      )}
+
+      {/* <img className="parallax__stars" src="./parallax/clouds_1.svg" alt="" />
       <img className="parallax__planets mobile-bottom" src="./parallax/clouds_2.svg" alt="" />
       <img className="parallax__mountain1" src="./parallax/clouds_3.svg" alt="" />
       <img className="parallax__mountain2 mobile-bottom" src="./parallax/clouds_4.svg" alt="" />
-      <img className="parallax__crater" src="./parallax/clouds_5.svg" alt="" />
+      <img className="parallax__crater" src="./parallax/clouds_5.svg" alt="" /> */}
 
-      <HouseCanvas scrollContainer={scrollContainer} />
+      <HouseCanvas />
     </section>
 
   )
--- personal-portfolio/src/sections/Navbar.tsx
+++ personal-portfolio/src/sections/Navbar.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { navLinks } from '../constants'
 import { twMerge } from 'tailwind-merge'
+import * as ReactUse from 'react-use'
+import gsap from 'gsap'
 
 
 
@@ -17,58 +19,76 @@ const NavItems = ({className}:{className?:string}) => {
 }
 
 const Navbar = () => {
-    const [isAudioPlaying,setIsAudioPlaying] = useState(false)
-    const [isIndicatorActive,setIndicatorActive] = useState(false)
+  // State for toggling audio and visual indicator
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [isIndicatorActive, setIsIndicatorActive] = useState(false);
 
-    const navContainerRef =  useRef(null)
-    const audioElementRef = useRef(null)
+  // Refs for audio and navigation container
+  const audioElementRef = useRef(null);
+  const navContainerRef = useRef(null);
 
-    const toggleAudioIndicator = () => {
-        setIsAudioPlaying((prev) => !prev)
-        setIndicatorActive((prev) => !prev)
+  const { y: currentScrollY } = ReactUse.useWindowScroll();
+  console.log(currentScrollY); 
+  const [isNavVisible, setIsNavVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  // Toggle audio and visual indicator
+  const toggleAudioIndicator = () => {
+    setIsAudioPlaying((prev) => !prev);
+    setIsIndicatorActive((prev) => !prev);
+  };
+
+  // Manage audio playback
+  useEffect(() => {
+    if (isAudioPlaying) {
+      audioElementRef.current.play();
+    } else {
+      audioElementRef.current.pause();
     }
-    
-    const [isOpen,setIsOpen] = useState(false)
-    const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen)
+  }, [isAudioPlaying]);
 
-    useEffect(()=>{
-        if(isAudioPlaying){
-            audioElementRef.current.play();
-        }else{
-            audioElementRef.current.pause();
-        }
-    },[isAudioPlaying])
+  useEffect(() => {
+    if (currentScrollY === 0) {
+      // Topmost position: show navbar without floating-nav
+      setIsNavVisible(true);
+      navContainerRef.current.classList.remove("floating-nav");
+    } else if (currentScrollY > lastScrollY) {
+      // Scrolling down: hide navbar and apply floating-nav
+      setIsNavVisible(false);
+      navContainerRef.current.classList.add("floating-nav");
+    } else if (currentScrollY < lastScrollY) {
+      // Scrolling up: show navbar with floating-nav
+      setIsNavVisible(true);
+      navContainerRef.current.classList.add("floating-nav");
+    }
+    console.log(currentScrollY)
+    setLastScrollY(currentScrollY);
+  }, [currentScrollY, lastScrollY]);
+
+  useEffect(() => {
+    console.log('ScrollY:', currentScrollY); // Check if the value is updating
+  }, [currentScrollY]);  // Logs whenever currentScrollY changes
+
+  useEffect(() => {
+    console.log('isNavVisible', isNavVisible);  // Debug the visibility state
+    gsap.to(navContainerRef.current, {
+      y: isNavVisible ? '0%' : '-120%',
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.2,
+    });
+  }, [isNavVisible]);
 
     return (
-        // <header className='fixed top-0 left-0 right-0 z-50 bg-[#1D2D44]'>
-        //     <div className='max-w-7xl mx-auto'>
-        //         <div className='flex justify-between items-center py-5 mx-auto sm:px-10 px-5'>
-        //             <a href="/" className='text-[#748CAB] font-bold text-xl hover:text-[#F0EBD8] transition-colors'>Rohan</a>
-        //             <button className='cursor-pointer text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' aria-label='Toggle Menu' onClick={toggleMenu}>
-        //                 <img src={isOpen? "assets/close.svg" : "assets/menu.svg"} alt="toggle" className='h-6 w-6' />
-        //             </button>
-        //             <nav className='sm:flex hidden'>
-        //                 <NavItems />
-        //             </nav>
-        //         </div>
-        //     </div>
-        //     <div className={`absolute left-0 right-0 bg-black-200 backdrop-blur-sm transition-all duration-300 ease-in-out overflow-hidden z-20 mx-auto sm:hidden block ${isOpen ? 'max-h-screen': 'max-h-0'}`}>
-        //         <nav className='p-5'>
-        //             <NavItems />
-        //         </nav>
-        //     </div>
-        // </header>
-        // <div className='flex justify-center items-center fixed top-3 z-40'>
-        <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'>
-            <header className='absolute top-1/2 w-full'>
-                <nav className='flex size-full items-center justify-between p-4'>
+        <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6 text-white'>
+            <header className='absolute top-1/2 w-full -translate-y-1/2'>
+                <nav className='flex size-full items-center justify-between p-4 '>
                     <div className='flex items-center gap-7'>
                         <img src='/assets/logo.png' alt='me' className='w-10' />
                     </div>
                     <div className='flex h-full items-center'>
                         <div className='hidden md:block'>
                             {navLinks.map((item)=>(
-                                <a href={`#${item.name.toLowerCase()}`}key={item.id} className='relative ms-10 font-general text-xs uppercase text-blue-50 after:absolute after:-bottom-0.5 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neutral-800 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100 dark:after:bg-white cursor-pointer'>
+                                <a href={`#${item.name.toLowerCase()}`}key={item.id} className='relative ms-10 font-general text-xs uppercase after:absolute after:-bottom-0.5 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neutral-800 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100 dark:after:bg-white cursor-pointer'>
                                     {item.name}
                                 </a>
                             ))}
